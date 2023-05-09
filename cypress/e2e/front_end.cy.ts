7d3ef50720d3b7601e76f145ff08437b1ec198fc
Devs Deck <prashanth.mani90@gmail.com>
2023-05-09 13:49:20 +0100

    Added Test Cases

--- cypress/e2e/front_end.cy.ts
+++ cypress/e2e/front_end.cy.ts
@@ -14,5 +14,28 @@ describe("Frontend Test Suite", () => {
     cy.get(".sh-site__logo").should("be.visible"); // replace with actual selector for the logo
   });
 
+  it("Go to Apps page and check the app links", () => {
+    cy.get("#button-307965270-1").should("be.visible"); // Check by Id, Whether the element is visible
+    cy.get("#button-307965270-1").click(); // Navigate to
+    cy.url().should("eq", "https://www.simplyhealth.co.uk/health-plan/apps");
+    cy.get(
+      'a.sh-link.app_store_link[href="https://apps.apple.com/gb/app/simplyhealth/id1484099929"]'
+    ).should("exist");
+    cy.get(
+      'a.sh-link.app_store_link[href="https://play.google.com/store/apps/details?id=co.uk.simplyhealth.app&hl=en_GB"]'
+    ).should("exist");
+  });
+
+  it("Go to My Accounts Login page", () => {
+    cy.get("a[href='https://www.simplyhealth.co.uk/member-portal']").should(
+      "be.visible"
+    );
+    cy.get("a[href='https://www.simplyhealth.co.uk/member-portal']").click();
+    cy.url().should("eq", "https://www.simplyhealth.co.uk/member-portal");``
+    cy.get('input[name="email"]').type("example@example.com");
+    cy.get('input[data-testid="general_password_password"]').type("Password@123");
+    cy.get('button[data-testid="Login_SignIn_Button"]').click();
+    cy.contains('Error: Incorrect username or password.').should('be.visible');
+  });
   // More frontend tests go here...
 });
