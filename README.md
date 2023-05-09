3ec79970af9f5f3af8a7f7e294cd8b8474f924cc
Devs Deck <prashanth.mani90@gmail.com>
2023-05-09 03:16:19 +0100

    Final Commit
    1. Added the Pet Api Sync in API Testing.
    2. Updating Readne.md for Getting started with project.

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # QA-Engineering-Challenge
+
+This project contains end-to-end (E2E) Cypress testing setup for frontend and API testing. The project uses Swagger Codegen to generate a TypeScript API client library for the Petstore API, which is used for testing the API endpoints.
+
+## Installation
+
+To install the project dependencies, run the following command:
+
+npm install
+
+This will install the required dev dependencies, including Cypress, Swagger Codegen, and TypeScript.
+
+# Generating the API Client Library
+
+If you need to regenerate the TypeScript API client library for the Petstore API, you can use the following command:
+
+npm run generate:api
+
+## Running the Tests
+
+### Frontend Tests
+To run the frontend tests, you can use the following command:
+
+npm run cypress:open
+
+This will open the Cypress Test Runner, which allows you to interactively run and debug the frontend tests.
+
+Alternatively, you can run the tests headlessly using the following command:
+
+npm run cypress:run
+
+This will run the tests in the background, without opening the Cypress Test Runner.
+
+### API Tests
+
+To run the API tests, you can use the following command:
+
+npm run cypress:spec
+
+This will run the Cypress tests located in the cypress/e2e directory. The tests use the generated TypeScript API client library to interact with the Petstore API and test the API endpoints.
+
+
+This will use Swagger Codegen to generate a new TypeScript API client library based on the Swagger YAML file located at https://petstore.swagger.io/v2/swagger.yaml. The generated library will be saved in the src/api directory.
--- cypress/e2e/api.cy.ts
+++ cypress/e2e/api.cy.ts
@@ -1,47 +1,46 @@
+import { Api, Pet } from "../../src/api/Api";
+
 /// <reference types="cypress" />
 
-type Pet = {
-  id: number;
-  name: string;
-  status: string;
-  category: any;
-  photoUrls: any;
-  tags: any;
-};
+const petApi = new Api();
 
 describe("API Test Suite", () => {
+  it("should return a list of pets", async () => {
+    return petApi.pet
+      .findPetsByStatus({
+        status: ["available"],
+      })
+      .then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.data.length).to.greaterThan(1);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
+
   it("creates a pet", () => {
     const newPet: Pet = {
       id: 1,
-      category: {
-        id: 0,
-        name: "Lab",
-      },
       name: "doggie",
       photoUrls: ["NA"],
-      tags: [
-        {
-          id: 0,
-          name: "Lab",
-        },
-      ],
       status: "available",
     };
-    cy.request("POST", "https://petstore.swagger.io/v2/pet", newPet).then(
-      (response) => {
-        console.log(response);
-        expect(response.status).to.eq(200);
-        expect(response.body.name).to.eq("doggie");
-      }
-    );
+    return petApi.pet.addPet(newPet).then((response) => {
+      expect(response.status).to.eq(200);
+      cy.log("Created successfully");
+    });
   });
   it("gets a pet by ID", () => {
-    cy.request("GET", "https://petstore.swagger.io/v2/pet/1").then(
-      (response) => {
+    return petApi.pet
+      .getPetById(1)
+      .then((response) => {
         expect(response.status).to.eq(200);
-        expect(response.body).to.have.property("id", 1);
-      }
-    );
+        expect(response.data.name).to.eq("doggie");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   });
 
   // More API tests go here...
